0a2af2512d8e4593f99963698e3e2fe0906b8686
Adeilson Silva <adeilson@gmail.com>
2019-10-03 22:06:03 -0300

    - Mobile

---
+++ mobile/src/pages/Book.js
@@ -0,0 +1,102 @@
+import React, { useState } from 'react';
+import {
+  SafeAreaView,
+  Text,
+  AsyncStorage,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Alert
+} from 'react-native';
+
+import api from '../services/api';
+
+export default ({ navigation }) => {
+  const [date, setDate] = useState('');
+
+  const id = navigation.getParam('id');
+
+  const handleSubmit = async () => {
+    const userId = await AsyncStorage.getItem('user');
+    await api.post(`/spots/${id}/bookings`, {
+      date
+    }, {
+      user_id: userId
+    });
+
+    Alert.alert('Solicitação de reserva enviada.');
+    navigation.navigate('List');
+  };
+
+  const handleCancel = async () => {
+    navigation.navigate('List');
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.label}>DATA DE INTERESSE *</Text>
+      <TextInput
+        style={styles.input}
+        placeholder='Qual data você quer reservar?'
+        placeholderTextColor='#999'
+        keyboardType='email-address'
+        autoCapitalize='none'
+        autoCorrect={false}
+        value={date}
+        onChangeText={setDate}
+      />
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={handleSubmit}
+      >
+        <Text style={styles.buttonText}>Encontrar spots</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[styles.button, styles.cancelButton]}
+        onPress={handleCancel}
+      >
+        <Text style={styles.buttonText}>Cancelar</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 30
+  },
+  label: {
+    fontWeight: 'bold',
+    color: '#444',
+    marginBottom: 8,
+    marginTop: 30
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    paddingHorizontal: 20,
+    fontSize: 16,
+    color: '#444',
+    height: 44,
+    marginBottom: 20,
+    borderRadius: 2
+  },
+  button: {
+    height: 42,
+    backgroundColor: '#f05a5b',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 2
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16
+  },
+  cancelButton: {
+    backgroundColor: '#ccc',
+    marginTop: 10
+  }
+});
---
+++ mobile/src/pages/Login.js
@@ -0,0 +1,115 @@
+import React, { useState, useEffect } from 'react';
+import {
+  View,
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  AsyncStorage
+} from 'react-native';
+
+import api from '../services/api';
+
+import logo from '../assets/logo.png';
+
+export default ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [techs, setTechs] = useState('');
+
+  useEffect(() => {
+    AsyncStorage.getItem('user').then(user => {
+      if (user) {
+        navigation.navigate('List');
+      }
+    });
+  }, []);
+
+  const handleSubmit = async () => {
+    const response = await api.post('/sessions', { email });
+    const { _id } = response.data;
+
+    await AsyncStorage.setItem('user', _id);
+    await AsyncStorage.setItem('techs', techs);
+    navigation.navigate('List');
+  };
+
+  return (
+    <KeyboardAvoidingView behavior='padding' style={styles.container}>
+      <Image source={logo} />
+
+      <View style={styles.form}>
+        <Text style={styles.label}>SEU E-MAIL *</Text>
+        <TextInput
+          style={styles.input}
+          placeholder='Seu e-mail'
+          placeholderTextColor='#999'
+          keyboardType='email-address'
+          autoCapitalize='none'
+          autoCorrect={false}
+          value={email}
+          onChangeText={setEmail}
+        />
+
+        <Text style={styles.label}>TECNOLOGIAS *</Text>
+        <TextInput
+          style={styles.input}
+          placeholder='Tecnologias de interesse'
+          placeholderTextColor='#999'
+          autoCapitalize='words'
+          autoCorrect={false}
+          value={techs}
+          onChangeText={setTechs}
+        />
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleSubmit}
+        >
+          <Text style={styles.buttonText}>Encontrar spots</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  label: {
+    fontWeight: 'bold',
+    color: '#444',
+    marginBottom: 8
+  },
+  form: {
+    alignSelf: 'stretch',
+    paddingHorizontal: 30,
+    marginTop: 30
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    paddingHorizontal: 20,
+    fontSize: 16,
+    color: '#444',
+    height: 44,
+    marginBottom: 20,
+    borderRadius: 2
+  },
+  button: {
+    height: 42,
+    backgroundColor: '#f05a5b',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 2
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16
+  }
+});
